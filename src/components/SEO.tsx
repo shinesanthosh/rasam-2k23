34367f6d8e3392b773fdeb7cd041f8251ad554a6
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-04-17 14:51:22 +0530

    Add twitter card to SEO

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -17,15 +17,26 @@ const SEO = ({
   return (
     <Head>
       <title>{title + ' | RASAM 2023'}</title>
+      {/* General Meta tags */}
       <meta charSet='UTF-8' />
       <meta name='description' content={description} />
       <meta name='keywords' content={keywords ? keywords : ''} />
+
+      {/* OG Tags */}
       <meta property='og:title' content={title + ' | RASAM 2023'} />
       <meta property='og:type' content='website' />
-      <meta property='og:image' content={imageURL ? imageURL : '/images/logo.png'} />
+      <meta
+        property='og:image'
+        content={imageURL ? imageURL : '/images/logo.png'}
+      />
       <meta property='og:description' content={description} />
       <meta property='og:url' content='/' />
-    <meta property='' />  
+      <meta property='' />
+
+      {/* Twitter card tags */}
+      <meta name='twitter:card' content='summary_large_image'></meta>
+
+
     </Head>
   )
 }
