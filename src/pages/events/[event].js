689ece511e5e5a345c44492ac4177187f3d47ffb
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-04-18 10:45:47 +0530

    Add guidline image to event

--- src/pages/events/[event].js
+++ src/pages/events/[event].js
@@ -21,7 +21,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }) => {
   const url = process.env.CMSURL
-  const query = `*[_type == 'events' && slug.current == '${params.event}']{ name,date,details,short,url, cords[]{name, email, contact}, image{asset->{url}}, meta{title, desc, image{asset->{url}}, keywords} }`
+  const query = `*[_type == 'events' && slug.current == '${params.event}']{ name,date,details,short,url, cords[]{name, email, contact}, image{asset->{url}}, guidelines{asset->{url}},meta{title, desc, image{asset->{url}}, keywords} }`
   const res = await fetch(url + encodeURIComponent(query))
   const data = await res.json()
 
@@ -38,39 +38,55 @@ const Event = ({ data }) => {
   Example data:
   
   {
-  cords: [
-    {
-      contact: '+9xxxxxxxxx4',
-      email: 'kevinxxxxxx@xxxxxxxx.in',
-      name: 'Kevin'
-    },
-    {
-      contact: '+91xxxxxxxxxxx9',
-      email: 'samsonxxxxxxxx@xxxxxxxxxx.in',
-      name: 'Samson'
-    }
-  ],
-  date: '2023-04-27',
-  details: "Bluetern is not just a football tournament; it's an experience that will leave participants exhilarated and inspired. The game requires quick reflexes, strategic thinking, and teamwork, making it a perfect test of skill for football enthusiasts. The intense 10-minute games will have you on the edge of your seat, as each team tries to outmaneuver the other and score as many goals as possible.",
-  image: {
-    asset: {
-      url: 'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-900x1600.jpg'
-    }
-  },
-  meta: {
-    desc: "Bluetern is a 3x3 football tournament that promises an exciting and adrenaline-filled experience. Join us for a unique celebration of the ocean and sports, where quick reflexes, strategic thinking, and teamwork are put to the test. Don't miss out on this unmissable opportunity to be a part of the most thrilling event on the planet!",
-    image: { 
+  
+    cords: [
+      {
+        contact: '+9xxxxxxxxx4',
+        email: 'kevinxxxxxx@xxxxxxxx.in',
+        name: 'Kevin'
+      },
+
+      {
+        contact: '+91xxxxxxxxxxx9',
+        email: 'samsonxxxxxxxx@xxxxxxxxxx.in',
+        name: 'Samson'
+      }
+    ],
+
+    date: '2023-04-27',
+    
+    details: "Bluetern is not just a football tournament; it's an experience that will leave participants exhilarated and inspired. The game requires quick reflexes, strategic thinking, and teamwork, making it a perfect test of skill for football enthusiasts. The intense 10-minute games will have you on the edge of your seat, as each team tries to outmaneuver the other and score as many goals as possible.",
+    
+    guidelines: {
       asset: {
         url: 'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-900x1600.jpg'
       }
-    
     },
-    keywords: 'football, rasam, festival',
-    title: 'Bluetern 3*3 Football'
-  },
-  name: 'BLUETERN 3*3 Football',
-  short: '3*3 Football',
-  url: 'https://google.com/'
+
+    image: {
+      asset: {
+        url: 'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-900x1600.jpg'
+      }
+    },
+
+    meta: {
+     
+      desc: "Bluetern is a 3x3 football tournament that promises an exciting and adrenaline-filled experience. Join us for a unique celebration of the ocean and sports, where quick reflexes, strategic thinking, and teamwork are put to the test. Don't miss out on this unmissable opportunity to be a part of the most thrilling event on the planet!",
+     
+      image: { 
+        asset: {
+          url: 'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-900x1600.jpg'
+        }
+      
+      },
+      keywords: 'football, rasam, festival',
+      
+      title: 'Bluetern 3*3 Football'
+    },
+    
+    name: 'BLUETERN 3*3 Football',
+    short: '3*3 Football',
+    url: 'https://google.com/'
 }
 
 
--- src/pages/procup.js
+++ src/pages/procup.js
@@ -1,15 +1,51 @@
-import React from "react";
-import styles from "../styles/Procup.module.scss";
-import NavBar from "../components/navbar";
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/Procup.module.scss'
+import NavBar from '../components/navbar'
+import SEO from '../components/SEO'
+import { data } from 'jquery'
 
 const Procup = () => {
+  const [events, setEvents] = useState([])
+
+  const getEvents = () => {
+    console.log('getting events')
+    fetch(process.env.NEXT_PUBLIC_GETEVENTS)
+      .then((res) => res.json())
+      .then((data) => {
+        let eventsList = data.events
+        eventsList.sort((a, b) => new Date(a.datetime) - new Date(b.datetime))
+        setEvents(eventsList)
+      })
+      .catch((e) => console.error(e))
+  }
+
+  useEffect(() => {
+    getEvents()
+  }, [])
+
+  let historyArr = []
+  events.slice(0, 3).map((event, key) => {
+    historyArr.push(
+      <p key={key}>
+        {event.events_teama.name} <span>{event.teama_score} </span>{' '}
+        <span>VS </span> {event.events_teamb.name}{' '}
+        <span> {event.teamb_score} </span>
+      </p>
+    )
+  })
+
   return (
     <div className={styles.procMain}>
+      <SEO
+        title='Providence Cup'
+        description='Live Scores and Updates. Providence Cup and George Mathew Memorial Trophie Tournaments'
+        imageURL='/images/proc.png'
+      />
       <NavBar />
       <div className={styles.procMask}>
         <div className={styles.imgContainer}>
-          <img src="/images/proc.png" alt="PROC LOGO" />
-          <img src="/images/gmmt.png" alt="GMMT LOGO" />
+          <img src='/images/proc.png' alt='PROC LOGO' />
+          <img src='/images/gmmt.png' alt='GMMT LOGO' />
         </div>
         <h1>PROCUP & GMMT</h1>
 
@@ -21,19 +57,12 @@ const Procup = () => {
 
         <h2>HISTORY</h2>
         {/* DATA HERE */}
-        <div className={styles.historyContainer}>
-          <p>
-            CEC <span>3</span> <span>VS </span>PRC-B <span>0</span>
-          </p>
-          <p>
-            CEC <span>3</span> <span>VS </span>PRC-B <span>0</span>
-          </p>
-        </div>
+        <div className={styles.historyContainer}>{historyArr}</div>
 
         <h2>UPCOMING</h2>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Procup;
+export default Procup
