42d773a5300f6d99962fc58cbe06edc1edf94e32
Jayasurya <jayasooryapj@gmail.com>
2023-04-19 15:36:46 +0530

    idk what commit

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import SEO from '../../components/SEO'
-import NavBar from '../../components/navbar'
-import styles from '../../styles/Events.module.scss'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React from "react";
+import Router from "next/router";
+import SEO from "../../components/SEO";
+import NavBar from "../../components/navbar";
+import styles from "../../styles/Events.module.scss";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const getStaticProps = async () => {
-  const url = process.env.CMSURL
-  const query = `*[_type == 'events']{name, short, date, slug{current},meta{image{asset->{url}}},image { asset -> {url}}}`
-  const res = await fetch(url + encodeURIComponent(query))
-  const data = await res.json()
+  const url = process.env.CMSURL;
+  const query = `*[_type == 'events']{name, short, date, slug{current},meta{image{asset->{url}}},image { asset -> {url}}}`;
+  const res = await fetch(url + encodeURIComponent(query));
+  const data = await res.json();
 
   return {
     props: { data: data.result },
-  }
-}
+  };
+};
 
 const Events = ({ data }) => {
   //  The data prop holds the array of events to be displayed
@@ -30,7 +31,7 @@ const Events = ({ data }) => {
   return (
     <>
       <SEO
-        title='Events'
+        title="Events"
         description="Welcome to Rasam, the ultimate fest that promises non-stop enjoyment and fun! Explore the list of events that we have in store for you, ranging from cultural activities to sports tournaments and tech competitions. Join us for 3 days of entertainment and laughter, and create unforgettable memories with your friends. Don't miss out on the excitement and register for your favorite events now!"
       />
 
@@ -43,26 +44,33 @@ const Events = ({ data }) => {
               {data.map((event, key) => {
                 return (
                   <Col className={styles.cardColumn} key={key}>
-                    <Card style={{ width: '18rem' }} className={styles.Card}>
+                    <Card style={{ width: "18rem" }} className={styles.Card}>
                       <Card.Img
-                        variant='top'
+                        variant="top"
                         src={event.meta.image.asset.url}
                       />
                       <Card.Body>
                         <Card.Title>{event.name.toUpperCase()}</Card.Title>
                         <Card.Text>{event.short}</Card.Text>
-                        <Button variant='outline-warning'>Know More</Button>
+                        <Button
+                          variant="outline-warning"
+                          onClick={() => {
+                            Router.push("/events/" + event.slug.current);
+                          }}
+                        >
+                          Know More
+                        </Button>
                       </Card.Body>
                     </Card>
                   </Col>
-                )
+                );
               })}
             </Row>
           </Container>
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
