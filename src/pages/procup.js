8abfe1ca5d3119cfdef68ae84698eab421ef346b
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-04-20 13:31:26 +0530

    Score update and auto live score

--- src/pages/procup.js
+++ src/pages/procup.js
@@ -1,56 +1,50 @@
-import React, { useEffect, useState } from "react";
-import styles from "../styles/Procup.module.scss";
-import NavBar from "../components/navbar";
-import SEO from "../components/SEO";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/Procup.module.scss'
+import NavBar from '../components/navbar'
+import SEO from '../components/SEO'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Procup = () => {
-  const [events, setEvents] = useState([]);
-
-  const getEvents = () => {
-    console.log("getting events");
-    fetch(process.env.NEXT_PUBLIC_GETEVENTS)
-      .then((res) => res.json())
-      .then((data) => {
-        let eventsList = data.events;
-        eventsList.sort((a, b) => new Date(a.datetime) - new Date(b.datetime));
-        setEvents(eventsList);
-      })
-      .catch((e) => console.error(e));
-  };
+  const [events, setEvents] = useState([])
 
   useEffect(() => {
-    getEvents();
-  }, []);
+    const intervalId = setInterval(getEvents, 15000)
 
-  let historyArr = [];
-  events.slice(0, 3).map((event, key) => {
-    historyArr.push(
-      <p key={key}>
-        {event.events_teama.name} <span>{event.teama_score} </span>{" "}
-        <span>VS </span> {event.events_teamb.name}{" "}
-        <span> {event.teamb_score} </span>
-      </p>
-    );
-  });
+    return () => clearInterval(intervalId)
+  }, [])
+
+  const getEvents = () => {
+    fetch(process.env.NEXT_PUBLIC_GETLIVEEVENTS, {
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_SECRET,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        let men = data.events[0]
+        setEvents(men)
+      })
+      .catch((e) => console.error(e))
+  }
 
   return (
     <>
       <NavBar />
       <main className={styles.procMain}>
         <SEO
-          title="Providence Cup"
-          description="Live Scores and Updates. Providence Cup and George Mathew Memorial Trophie Tournaments"
-          imageURL="/images/proc.png"
+          title='Providence Cup'
+          description='Live Scores and Updates. Providence Cup and George Mathew Memorial Trophie Tournaments'
+          imageURL='/images/proc.png'
         />
         <div className={styles.procMask}>
           <Container fluid className={styles.procContainer}>
             <Row className={styles.imgRow}>
               <Col className={styles.procCol}>
-                <img src="/images/proc.png" alt="PROC LOGO" />
-                <img src="/images/gmmt.png" alt="GMMT LOGO" />
+                <img src='/images/proc.png' alt='PROC LOGO' />
+                <img src='/images/gmmt.png' alt='GMMT LOGO' />
               </Col>
             </Row>
             <Row className={styles.titleRow}>
@@ -62,17 +56,17 @@ const Procup = () => {
                   <h4>GMMT Women&apos;s Basketball</h4>
                   <div className={styles.teamInfo}>
                     {/* TEAM NAME SHOULD GO HERE */}
-                    <h2>ABC</h2>
+                    <h2>ACC</h2>
                     <h1>VS</h1>
-                    <h2>DEF</h2>
+                    <h2>AJCE</h2>
                   </div>
                   <div className={styles.scoreBoard}>
                     {/* SCORE SHOULD GO HERE */}
                     <div className={styles.homeScore}>
-                      <h1>1</h1>
+                      <h1>29</h1>
                     </div>
                     <div className={styles.awayScore}>
-                      <h1>5</h1>
+                      <h1>12</h1>
                     </div>
                   </div>
                 </div>
@@ -81,17 +75,21 @@ const Procup = () => {
                 <div className={styles.card}>
                   <h4>ProCup Men&apos;s Football</h4>
                   <div className={styles.teamInfo}>
-                    <h2>ABC</h2>
+                    <h2>
+                      {events.events_teama ? events.events_teama.name : '---'}
+                    </h2>
                     <h1>VS</h1>
-                    <h2>DEF</h2>
+                    <h2>
+                      {events.events_teamb ? events.events_teamb.name : '---'}
+                    </h2>
                   </div>
                   <div className={styles.scoreBoard}>
                     {/* SCORE SHOULD GO HERE */}
                     <div className={styles.homeScore}>
-                      <h1>1</h1>
+                      <h1>{events.teama_score}</h1>
                     </div>
                     <div className={styles.awayScore}>
-                      <h1>5</h1>
+                      <h1>{events.teamb_score}</h1>
                     </div>
                   </div>
                 </div>
@@ -100,17 +98,17 @@ const Procup = () => {
                 <div className={styles.card}>
                   <h4>GMMT Men&apos;s Basketball</h4>
                   <div className={styles.teamInfo}>
-                    <h2>ABC</h2>
+                    <h2>SAHRDAYA</h2>
                     <h1>VS</h1>
-                    <h2>DEF</h2>
+                    <h2>KE</h2>
                   </div>
                   <div className={styles.scoreBoard}>
                     {/* SCORE SHOULD GO HERE */}
                     <div className={styles.homeScore}>
-                      <h1>1</h1>
+                      <h1>71</h1>
                     </div>
                     <div className={styles.awayScore}>
-                      <h1>5</h1>
+                      <h1>50  </h1>
                     </div>
                   </div>
                 </div>
@@ -125,7 +123,7 @@ const Procup = () => {
         </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Procup;
+export default Procup
