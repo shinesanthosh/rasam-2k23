e035cbfde56cbc7b445a9766345f66ede37a8d0c
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-04-21 12:30:31 +0530

    Add technical events data to events home

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,27 +1,31 @@
-import React from "react";
-import Router from "next/router";
-import SEO from "../../components/SEO";
-import NavBar from "../../components/navbar";
-import styles from "../../styles/Events.module.scss";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React from 'react'
+import Router from 'next/router'
+import SEO from '../../components/SEO'
+import NavBar from '../../components/navbar'
+import styles from '../../styles/Events.module.scss'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export const getStaticProps = async () => {
-  const url = process.env.CMSURL;
-  const query = `*[_type == 'events']{name, short, date, slug{current},meta{image{asset->{url}}},image { asset -> {url}}}`;
-  const res = await fetch(url + encodeURIComponent(query));
-  const data = await res.json();
+  let query = `*[_type == 'events' && type != 'tech']{name, short, date, slug{current},meta{image{asset->{url}}},image { asset -> {url}}}`
+  let res = await fetch(url + encodeURIComponent(query))
+  const data = await res.json()
+
+  query = `*[_type == 'events' && type == 'tech']{name, short, date, slug{current},meta{image{asset->{url}}},image { asset -> {url}}, type}`
+  res = await fetch(url + encodeURIComponent(query))
+  const techData = await res.json()
 
   return {
-    props: { data: data.result },
-  };
-};
+    props: { data: data.result, techData: techData.result },
+  }
+}
 
-const Events = ({ data }) => {
+const Events = ({ data, techData }) => {
   //  The data prop holds the array of events to be displayed
+  // Techdata holds technical events
   //  The array contains:
   // date: data[i].date
   // image url : data[i].image.asset.url
@@ -31,7 +35,7 @@ const Events = ({ data }) => {
   return (
     <>
       <SEO
-        title="Events"
+        title='Events'
         description="Welcome to Rasam, the ultimate fest that promises non-stop enjoyment and fun! Explore the list of events that we have in store for you, ranging from cultural activities to sports tournaments and tech competitions. Join us for 3 days of entertainment and laughter, and create unforgettable memories with your friends. Don't miss out on the excitement and register for your favorite events now!"
       />
 
@@ -44,33 +48,32 @@ const Events = ({ data }) => {
               {data.map((event, key) => {
                 return (
                   <Col className={styles.cardColumn} key={key}>
-                    <Card style={{ width: "18rem" }} className={styles.Card}>
+                    <Card style={{ width: '18rem' }} className={styles.Card}>
                       <Card.Img
-                        variant="top"
+                        variant='top'
                         src={event.meta.image.asset.url}
                       />
                       <Card.Body>
                         <Card.Title>{event.name.toUpperCase()}</Card.Title>
                         <Card.Text>{event.short}</Card.Text>
                         <Button
-                          variant="outline-warning"
+                          variant='outline-warning'
                           onClick={() => {
-                            Router.push("/events/" + event.slug.current);
-                          }}
-                        >
+                            Router.push('/events/' + event.slug.current)
+                          }}>
                           Know More
                         </Button>
                       </Card.Body>
                     </Card>
                   </Col>
-                );
+                )
               })}
             </Row>
           </Container>
         </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Events;
+export default Events
