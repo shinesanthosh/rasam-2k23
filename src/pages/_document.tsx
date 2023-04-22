7bd2014594dfc8b8d2346d92e622662b455c1294
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-04-22 10:18:50 +0530

    Add analytics to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <Script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-9C3HWRW5DH'
+        strategy='afterInteractive'></Script>
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9C3HWRW5DH');`}
+      </Script>
         <link rel="icon" href="/images/logo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
