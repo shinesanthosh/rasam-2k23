1674fcd1fea7f9d6f56cc9c833299b6eaee1b7f5
Jayasurya <jayasooryapj@gmail.com>
2023-04-25 12:07:31 +0530

    login form

--- src/pages/vip.js
+++ src/pages/vip.js
@@ -1,50 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import styles from "../styles/Login.module.scss";
 
 const Vip = () => {
-  const [coupon, setCoupon] = useState('')
-  const [email, setEmail] = useState('')
-  const [responseMessage, setResponseMessage] = useState('')
+  const [coupon, setCoupon] = useState("");
+  const [email, setEmail] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
 
   const handleSubmit = async () => {
-    const regex = /^([a-zA-Z0-9._-]+)@([a-zA-Z0-9_-]+)\.([a-zA-Z]{2,6})$/
+    const regex = /^([a-zA-Z0-9._-]+)@([a-zA-Z0-9_-]+)\.([a-zA-Z]{2,6})$/;
     if (regex.test(email)) {
-      const res = await fetch('/api/getCoupon', {
-        method: 'POST',
+      const res = await fetch("/api/getCoupon", {
+        method: "POST",
         body: JSON.stringify({ coupon, email }),
-      })
+      });
 
-      const data = await res.json()
-      if (data.status == 'error') setResponseMessage(data.message)
-      else if (data.status == 'success') {
-        setResponseMessage('Your ticket is reserved ')
-        setCoupon('')
-        setEmail('')
+      const data = await res.json();
+      if (data.status == "error") setResponseMessage(data.message);
+      else if (data.status == "success") {
+        setResponseMessage("Your ticket is reserved ");
+        setCoupon("");
+        setEmail("");
       }
     } else {
-      setResponseMessage('Invalid email')
+      setResponseMessage("Invalid email");
     }
-  }
+  };
 
   return (
-    <div>
-      <label>Coupon Code:</label>
-      <input
-        type='text'
-        name='coupon'
-        value={coupon}
-        onChange={e => setCoupon(e.target.value)}
-      />
-      <label>Email</label>
-      <input
-        type='email'
-        name='email'
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <button onClick={() => handleSubmit()}>Get ticket</button>
-      <span>{responseMessage}</span>
-    </div>
-  )
-}
+    <main className={styles.loginMain}>
+      <div className={styles.loginMask}>
+        <div className={styles.form}>
+          <h1>RASAM VIP PASS</h1>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Coupon Code"
+            className={styles.formInput}
+          >
+            <Form.Control type="email" placeholder="name@example.com" />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Enter your name"
+            className={styles.formInput}
+          >
+            <Form.Control type="text" placeholder="Gopika Ghosh" />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingPassword"
+            label="Password"
+            className={styles.formInput}
+          >
+            <Form.Control type="password" placeholder="Password" />
+          </FloatingLabel>
+          <Button variant="primary" className={styles.loginBtn}>
+            Book Ticket
+          </Button>{" "}
+          {/* <label>Coupon Code:</label>
+          <input
+            type="text"
+            name="coupon"
+            value={coupon}
+            onChange={(e) => setCoupon(e.target.value)}
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          /> */}
+          {/* <button onClick={() => handleSubmit()}>Get ticket</button> */}
+          <span>{responseMessage}</span>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Vip
+export default Vip;
